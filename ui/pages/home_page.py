6ee2bb3932c7338ba36f568f209a600060994035
Patrick Guis <guilhermepatrick394@gmail.com>
2026-07-03 17:04:24 -0300

    feat: UI tests

--- ui/pages/home_page.py
+++ ui/pages/home_page.py
@@ -4,12 +4,16 @@ from PySide6.QtWidgets import (
     QVBoxLayout, QPushButton, QLabel
 )
 
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\908430\Desktop\calculadoraCient\telemetry_calc\files\formulaOneLaps.csv')
+
 # 1. Todo app PySide6 precisa iniciar com o QApplication
 app = QApplication(sys.argv)
 
 # 2. Criamos a Janela Principal (Moldura do Software)
 janela_principal = QMainWindow()
-janela_principal.setWindowTitle("Meu Guia de Estudos PySide6")
+janela_principal.setWindowTitle("Telemetry Analysis Tool")
 janela_principal.resize(400, 200) # Largura e altura inicial
 
 # 3. Criamos um QWidget para ser a "Área Central" da janela
@@ -18,14 +22,22 @@ widget_central = QWidget()
 janela_principal.setCentralWidget(widget_central)
 
 # 4. Criamos os Componentes Visuais (Widgets)
-texto_informativo = QLabel("Clique no botão para atualizar o status.")
-botao_acao = QPushButton("Clique Aqui")
+texto_informativo = QLabel("Choose a option.")
+botao_um = QPushButton("test csv")
+botao_dois = QPushButton("Analysis")
+botao_tres = QPushButton("Compare")
+botao_quatro = QPushButton("Graphs")
+botao_cinco = QPushButton("Exit")
 
 # 5. Criamos o Organizador (Layout) e adicionamos os componentes nele
 # O QVBoxLayout vai alinhar o texto e o botão um embaixo do outro
 layout_vertical = QVBoxLayout()
 layout_vertical.addWidget(texto_informativo)
-layout_vertical.addWidget(botao_acao)
+layout_vertical.addWidget(botao_um)
+layout_vertical.addWidget(botao_dois)
+layout_vertical.addWidget(botao_tres)
+layout_vertical.addWidget(botao_quatro)
+layout_vertical.addWidget(botao_cinco)
 
 # 6. Atribuímos o layout para o nosso widget central
 widget_central.setLayout(layout_vertical)
@@ -33,14 +45,13 @@ widget_central.setLayout(layout_vertical)
 # --- SISTEMA DE EVENTOS (Signals & Slots) ---
 # Esta função será chamada quando o usuário interagir com o botão
 def ao_clicar_no_botao():
-    texto_informativo.setText("O botão foi clicado com sucesso! 🎉")
+    texto_informativo.setText(df.head(5))
     # Usando uma função exclusiva da QMainWindow (Barra de status no rodapé)
     janela_principal.statusBar().showMessage("Ação executada!", 3000) # Some em 3 segundos
 
-# Conectamos o sinal de clique do QPushButton à nossa função Python
-botao_acao.clicked.connect(ao_clicar_no_botao)
-# --------------------------------------------
+def ao_clicar_em_sair():
+    janela_principal.show()
+    sys.exit(app.exec())
 
-# 7. Exibimos a janela e iniciamos o loop de execução do programa
-janela_principal.show()
-sys.exit(app.exec())
+botao_um.clicked.connect(ao_clicar_no_botao)
+botao_cinco.clicked.connect(ao_clicar_em_sair)
